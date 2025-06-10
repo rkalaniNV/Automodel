0b6f0ff6eda3eebc0413c04bd72db4e5fa206870
Dong Hyuk Chang <thomaschang26@tutanota.com>
2025-06-10 10:29:03 -0400

    Add finetune functional tests (#44)
    * Add finetune functional tests
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Gate preflight on Run CICD
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Test preflight gate
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Skip Run CICD label
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Use relative actions pathing
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Add checkout to e2e tests
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Add wandb to requirements
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * add missing set_by_dotted
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * update test
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * refactor step_scheduler to wrap dataloader
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * lint
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * fix path
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * update dataset path
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * limit samples to 10
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Log test vars
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Add placeholder tests to validate ci
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Add finetune functional tests
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Gate preflight on Run CICD
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Test preflight gate
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Skip Run CICD label
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Use relative actions pathing
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Add checkout to e2e tests
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * update test
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * update dataset path
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * limit samples to 10
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * fix
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Test placeholder on gpu
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Update tools coverage paths in pyproject
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Pip install prior to launching tests
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * fix
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    * Update auto_model.py
    
    * Exit with exit code 0 if end of the script
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Gracefully fail on coverage xml
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Temporarily skip coverage
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Remove placeholder tests
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    * Remove placeholder and add back functional test
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    
    ---------
    
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    Co-authored-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    Co-authored-by: Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>

--- recipes/llm/finetune.py
+++ recipes/llm/finetune.py
@@ -114,6 +114,8 @@ def build_dataloader(cfg_ds, cfg_dl, cfg_model, distributed_sampler_kwargs) -> D
     """
     if not 'tokenizer' in cfg_ds:
         tokenizer = AutoTokenizer.from_pretrained(cfg_model.pretrained_model_name_or_path)
+    elif not '_target_' in cfg_ds.tokenizer:
+        tokenizer = AutoTokenizer.from_pretrained(**cfg_ds.tokenizer.to_dict())
     else:
         tokenizer = cfg_ds.tokenizer.instantiate()
     ds = cfg_ds.instantiate(tokenizer=tokenizer)
