d79ac0a6099c0f4ad17e93822336766b16e0f0a9
Huiying <willwin.lee@gmail.com>
2025-07-15 15:32:58 -0700

    fix: load model and loss mask fix (#195)
    * multiple fixes
    
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>
    
    * update fixture
    
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>
    
    * revert change in nemo_automodel
    
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>
    
    * revert
    
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>
    
    ---------
    
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>

--- nemo_automodel/datasets/vlm/collate_fns.py
+++ nemo_automodel/datasets/vlm/collate_fns.py
@@ -50,9 +50,7 @@ def create_loss_mask_with_start_of_response_token(input_ids, processor, start_of
         start_of_turn_token_id = start_of_response_token_id[0]
     if isinstance(start_of_response_token, str) and input_ids.count(start_of_turn_token_id) >= 2:
         first_start_of_turn_token_id = input_ids.index(start_of_turn_token_id)
-        response_start = input_ids.index(start_of_turn_token_id, first_start_of_turn_token_id + 1) + len(
-            start_of_response_token_id
-        )
+        response_start = input_ids.index(start_of_turn_token_id, first_start_of_turn_token_id + 1)
     else:
         response_start = 0
 
