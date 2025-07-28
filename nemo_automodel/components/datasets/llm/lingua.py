9dde4fcc947de3a3e7578a0076fd35ce9b22e2f0
adil-a <adil.asif2000@hotmail.com>
2025-07-28 13:07:10 +0000

    fixing batches
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- nemo_automodel/components/datasets/llm/lingua.py
+++ nemo_automodel/components/datasets/llm/lingua.py
@@ -2,6 +2,7 @@ from typing import TYPE_CHECKING
 from torch.utils.data import IterableDataset
 from nemo_automodel.components.datasets.llm.lingua_assets.core import build_dataloader_from_args, init_dataloader_state_from_args
 from contextlib import ExitStack
+import torch
 
 if TYPE_CHECKING:
     from transformers.tokenization_utils_base import PreTrainedTokenizerBase
@@ -23,7 +24,15 @@ class JSONLDataset(IterableDataset):
 
     def __iter__(self):
         for batch, state in self.data_loader:
-            yield batch
+            batch = torch.from_numpy(batch)
+            bs, s, _ = batch.shape
+            loss_mask = torch.Tensor([1 for _ in range(s)]).unsqueeze(0).repeat(bs, 1).to(torch.int64)
+            return_batch = {
+                "input_ids": batch[:, :, 0],
+                "labels": batch[:, :, 1],
+                "loss_mask": loss_mask,
+            }
+            yield return_batch
             self.data_loader_state = state
     
     def __del__(self):
