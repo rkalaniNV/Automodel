90436ec169b87e56880bc3979f7d5ac0a204b053
adil-a <adil.asif2000@hotmail.com>
2025-08-08 16:46:42 +0000

    attribution
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- nemo_automodel/components/checkpoint/_backports/dtensor_backports.py
+++ nemo_automodel/components/checkpoint/_backports/dtensor_backports.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# taken from https://github.com/pytorch/pytorch/issues/147724
+
 from typing import cast
 
 import torch
