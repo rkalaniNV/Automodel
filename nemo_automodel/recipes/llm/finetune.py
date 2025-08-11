f1f70867594568f7be5f311b5d7cb881d7e1b5ac
Hemil Desai <hemild@nvidia.com>
2025-08-10 17:19:00 -0700

    fix
    Signed-off-by: Hemil Desai <hemild@nvidia.com>

--- nemo_automodel/recipes/llm/finetune.py
+++ nemo_automodel/recipes/llm/finetune.py
@@ -655,7 +655,12 @@ class FinetuneRecipeForNextTokenPrediction(BaseRecipe):
             and self.device_mesh is not None
             and (self.device_mesh["cp"].size() > 1 or self.device_mesh["tp"].size() > 1)
         ):
-            batch["position_ids"] = torch.arange(0, batch["input_ids"].shape[1]).unsqueeze(0).to(self.model.device)
+            batch["position_ids"] = (
+                torch.arange(0, batch["input_ids"].shape[1])
+                .unsqueeze(0)
+                .expand(batch["input_ids"].shape[0], -1)
+                .to(self.model.device)
+            )
 
         train_ctx, batch = make_cp_batch_and_ctx(self.device_mesh, batch, labels, loss_mask)
         if self.pp_enabled:
