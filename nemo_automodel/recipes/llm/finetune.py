fix: scaling loss in multinode training (#376)
* fix scaling

Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

* fix scaling

Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

* fix valid

Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

* fix

Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

* adding correct loss scaling for LLMs

Signed-off-by: adil-a <adil.asif2000@hotmail.com>

* reverting to main until training is fixed

Signed-off-by: adil-a <adil.asif2000@hotmail.com>

* linting

Signed-off-by: adil-a <adil.asif2000@hotmail.com>

* removing

Signed-off-by: adil-a <adil.asif2000@hotmail.com>

* reverting step scheduler change

Signed-off-by: adil-a <adil.asif2000@hotmail.com>

* linting

Signed-off-by: adil-a <adil.asif2000@hotmail.com>

* reverting

Signed-off-by: adil-a <adil.asif2000@hotmail.com>

* reverting

Signed-off-by: adil-a <adil.asif2000@hotmail.com>

* reverting

Signed-off-by: adil-a <adil.asif2000@hotmail.com>

* use dtype in tensors

Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

* fmt

Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

* fix

Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

* linting

Signed-off-by: adil-a <adil.asif2000@hotmail.com>

---------

Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
Signed-off-by: adil-a <adil.asif2000@hotmail.com>
Co-authored-by: adil-a <adil.asif2000@hotmail.com>

nemo_automodel/recipes/llm/finetune.py
@@ -620,12 +620,19 @@ class FinetuneRecipeForNextTokenPrediction(BaseRecipe):
             max_grad_norm: Gradient clipping norm. Optional, if None will not clip gradients.
         """
 
-        num_label_tokens = sum((batch["labels"] != -100).sum().item() for batch in batches)
+        num_label_tokens = torch.tensor(
+            sum((batch["labels"] != -100).sum().item() for batch in batches), dtype=torch.long
+        )
+        num_label_tokens = self._dp_allreduce(num_label_tokens).item()
         loss_buffer = []
 
         # number of tokens in the batch, excluding any tail padding.
-        num_tokens_in_batch = sum(batch["labels"].numel() - count_tail_padding(batch["labels"]) for batch in batches)
-        num_tokens_in_batch = self._dp_allreduce(torch.LongTensor([num_tokens_in_batch])).item()
+        num_tokens_in_batch = torch.tensor(
+            sum(batch["labels"].numel() - count_tail_padding(batch["labels"]) for batch in batches),
+            dtype=torch.long,
+        )
+        num_tokens_in_batch = self._dp_allreduce(num_tokens_in_batch).item()
+        dp_group_size = self._get_dp_group_size()
 
         num_batches = len(batches)
         for i, batch in enumerate(batches):
@@ -637,7 +644,9 @@ class FinetuneRecipeForNextTokenPrediction(BaseRecipe):
                 and self.device_mesh is not None
                 and (self.device_mesh["cp"].size() > 1 or self.device_mesh["tp"].size() > 1)
             ):
-                batch["position_ids"] = torch.arange(0, batch["input_ids"].shape[1]).unsqueeze(0).to(self.model.device)
+                batch["position_ids"] = (
+                    torch.arange(0, batch["input_ids"].shape[1]).unsqueeze(0).to(self.dist_env.device)
+                )
 
             train_ctx, batch = make_cp_batch_and_ctx(self.device_mesh, batch, labels)
 
@@ -661,16 +670,19 @@ class FinetuneRecipeForNextTokenPrediction(BaseRecipe):
                     num_label_tokens=num_label_tokens,
                 )
                 loss_buffer.append(local_loss.clone().detach())
-                local_loss.backward()
+                (local_loss * dp_group_size).backward()
 
         # do the optimization step
         grad_norm = 0
-        # Clip gradients **after** any rescaling.
         # TODO(@boxiangw): Fix TP gradient clipping
         if max_grad_norm is not None and (not self.device_mesh or self.device_mesh["tp"].size() == 1):
             grad_norm = torch.nn.utils.clip_grad_norm_(
                 [p for p in self.model.parameters() if p.requires_grad], max_grad_norm
             )
+
+            if hasattr(grad_norm, "full_tensor"):
+                grad_norm = grad_norm.full_tensor()  # collect the summed grad norm across ranks
+
             if isinstance(grad_norm, torch.Tensor):
                 grad_norm = grad_norm.item()
 
@@ -702,7 +714,8 @@ class FinetuneRecipeForNextTokenPrediction(BaseRecipe):
         time_delta = t - self.timestamp
         self.timestamp = t
         tps = num_tokens_in_batch / time_delta
-        reporting_loss = torch.sum(torch.stack(loss_buffer)).item()
+        reporting_loss = torch.sum(torch.stack(loss_buffer))
+        reporting_loss = self._dp_allreduce(reporting_loss).item()
         # fix reporting_loss, tps across ranks
         return reporting_loss, grad_norm, tps, num_tokens_in_batch, num_label_tokens
 
@@ -712,13 +725,13 @@ class FinetuneRecipeForNextTokenPrediction(BaseRecipe):
         with StatefulRNG(seed=1, ranked=True):
             self.model.eval()
 
-            total_loss = 0.0
-            total_tokens = 0
+            total_loss = torch.tensor(0.0, dtype=torch.float32, device=self.dist_env.device)
+            total_num_label_tokens = 0
 
             for batch in self.val_dataloader:
                 batch = {k: v.to(self.dist_env.device, non_blocking=True) for k, v in batch.items()}
                 labels = batch.pop("labels")
-                num_label_tokens = (labels != -100).sum()
+                num_label_tokens = (labels != -100).sum().item()
 
                 if (
                     self.device_mesh
@@ -726,38 +739,40 @@ class FinetuneRecipeForNextTokenPrediction(BaseRecipe):
                     and (self.device_mesh["cp"].size() > 1 or self.device_mesh["tp"].size() > 1)
                 ):
                     batch["position_ids"] = (
-                        torch.arange(0, batch["input_ids"].shape[1]).unsqueeze(0).to(self.model.device)
+                        torch.arange(0, batch["input_ids"].shape[1]).unsqueeze(0).to(self.dist_env.device)
                     )
 
                 train_ctx, batch = make_cp_batch_and_ctx(self.device_mesh, batch, labels)
                 with train_ctx():
                     if isinstance(self.loss_fn, FusedLinearCrossEntropy):
-                        out = self.model(logits_to_keep=1, **batch)
-                    else:
-                        out = self.model(**batch)
-                    local_loss = calculate_loss(
+                        batch["logits_to_keep"] = 1
+                    out = self.model(**batch)
+                    total_loss += calculate_loss(
                         self.loss_fn,
                         logits=out.logits,
                         labels=labels,
                         model=self.model,
                         hidden_states=out.hidden_states[-1] if "hidden_states" in out else None,
-                        num_label_tokens=num_label_tokens,
+                        num_label_tokens=None,  # we will normalize outside.
                     )
 
-                total_loss += local_loss.item()
-                total_tokens += num_label_tokens
+                total_num_label_tokens += num_label_tokens
 
-        total_loss = self._dp_allreduce(torch.FloatTensor([total_loss])).item()
-        total_tokens = self._dp_allreduce(torch.LongTensor([total_tokens])).item()
+        total_loss = self._dp_allreduce(total_loss).item()
+        total_num_label_tokens = self._dp_allreduce(torch.tensor(total_num_label_tokens, dtype=torch.long)).item()
 
-        val_loss = total_loss / max(total_tokens, 1e-8)
+        val_loss = total_loss / max(total_num_label_tokens, 1e-8)
         if self.dist_env.is_main:
             if wandb.run is not None:
                 wandb.log({"val_loss": val_loss, "step": self.step_scheduler.step, "epoch": self.step_scheduler.epoch})
         current_lr = self.optimizer.param_groups[0]["lr"]
         logging.info(
-            "[val] step {} | epoch {} | loss {:.4f} | lr {:.2e}".format(
-                self.step_scheduler.step, self.step_scheduler.epoch, val_loss, current_lr
+            "[val] step {} | epoch {} | loss {:.4f} | lr {:.2e} | num_label_tokens {}".format(
+                self.step_scheduler.step,
+                self.step_scheduler.epoch,
+                val_loss,
+                current_lr,
+                total_num_label_tokens,
             )
         )
 