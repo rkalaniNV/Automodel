48c7e5aac4a21cededbde5791bef9ae3ef59b22e
Boxiang Wang <boxiangw@nvidia.com>
2025-08-23 21:36:11 -0700

    feat: Support HF TP_plan for FSDP managers (#312)
    * Add use_hf_tp_plan back to fsdp managers
    
    Signed-off-by: Boxiang Wang <boxiangw@nvidia.com>
    
    ---------
    
    Signed-off-by: Boxiang Wang <boxiangw@nvidia.com>
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    Co-authored-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

---
+++ tests/functional_tests/hf_transformer_llm/L2_HF_Transformer_LLM_FSDP2_TP2_HF_TPPLAN.sh
@@ -0,0 +1,37 @@
+# Copyright (c) 2020-2025, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+set -xeuo pipefail # Exit immediately if a command exits with a non-zero status
+
+TRANSFORMERS_OFFLINE=1 python -m torch.distributed.run --nproc_per_node=2 --nnodes=1 -m coverage run --data-file=/workspace/.coverage --source=/workspace/ --parallel-mode \
+-m pytest examples/llm/finetune.py \
+  --config examples/llm/llama_3_2_1b_squad.yaml \
+  --model.pretrained_model_name_or_path /home/TestData/akoumparouli/hf_mixtral_2l/ \
+  --step_scheduler.max_steps 3 \
+  --step_scheduler.grad_acc_steps 1 \
+  --step_scheduler.val_every_steps 1 \
+  --loss_fn._target_ nemo_automodel.components.loss.masked_ce.MaskedCrossEntropy \
+  --dataset.tokenizer.pretrained_model_name_or_path /home/TestData/akoumparouli/hf_mixtral_2l/ \
+  --validation_dataset.tokenizer.pretrained_model_name_or_path /home/TestData/akoumparouli/hf_mixtral_2l/ \
+  --dataset.dataset_name /home/TestData/lite/hf_cache/squad/ \
+  --dataset.limit_dataset_samples 10 \
+  --validation_dataset.dataset_name /home/TestData/lite/hf_cache/squad/ \
+  --validation_dataset.limit_dataset_samples 10 \
+  --distributed._target_ nemo_automodel.components.distributed.fsdp2.FSDP2Manager \
+  --distributed.dp_size none \
+  --distributed.tp_size 2 \
+  --distributed.cp_size 1 \
+  --distributed.sequence_parallel false \
+  --distributed.use_hf_tp_plan true
--- tests/functional_tests/hf_transformer_llm/test_hf_transformer_llm.py
+++ tests/functional_tests/hf_transformer_llm/test_hf_transformer_llm.py
@@ -18,14 +18,25 @@ from tests.utils.test_utils import run_test_script
 
 TEST_FOLDER = "hf_transformer_llm"
 HF_TRANSFORMER_LLM_FSDP2_TP2_FILENAME = "L2_HF_Transformer_LLM_FSDP2_TP2.sh"
+HF_TRANSFORMER_LLM_FSDP2_TP2_HF_TPPLAN_FILENAME = "L2_HF_Transformer_LLM_FSDP2_TP2_HF_TPPLAN.sh"
 HF_TRANSFORMER_LLM_NVFSDP_TP2_FILENAME = "L2_HF_Transformer_LLM_nvFSDP_TP2.sh"
+HF_TRANSFORMER_LLM_NVFSDP_TP2_HF_TPPLAN_FILENAME = "L2_HF_Transformer_LLM_nvFSDP_TP2_HF_TPPLAN.sh"
+
 
 
 class TestHFTransformerLLM:
     @pytest.mark.pleasefixme
     def test_hf_transformer_llm_fsdp2_tp2(self):
         run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_FSDP2_TP2_FILENAME)
+    
+    @pytest.mark.pleasefixme
+    def test_hf_transformer_llm_fsdp2_tp2_hf_tpplan(self):
+        run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_FSDP2_TP2_HF_TPPLAN_FILENAME)
 
     @pytest.mark.pleasefixme
     def test_hf_transformer_llm_nvfsdp_tp2(self):
         run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_NVFSDP_TP2_FILENAME)
+    
+    @pytest.mark.pleasefixme
+    def test_hf_transformer_llm_nvfsdp_tp2_hf_tpplan(self):
+        run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_NVFSDP_TP2_HF_TPPLAN_FILENAME)
