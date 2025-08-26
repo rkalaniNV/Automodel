987dcafbdd42da6e93266d4e0226443abef3fa01
Felipe Vieira Frujeri <ffrujeri@nvidia.com>
2025-08-26 10:00:32 -0700

    Add Automodel Factory and model_class resolution from model_type.
    Signed-off-by: Felipe Vieira Frujeri <ffrujeri@nvidia.com>

--- nemo_automodel/components/_transformers/utils.py
+++ nemo_automodel/components/_transformers/utils.py
@@ -12,9 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections import defaultdict
 from typing import Any
 
-from transformers import AutoConfig
+from torch import nn
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoModelForImageTextToText,
+    AutoModelForTextToWaveform,
+)
+
+# an automodel factory for loading the huggingface models from correct class
+AUTOMODEL_FACTORY = defaultdict(lambda: AutoModelForCausalLM)
+AUTOMODEL_FACTORY["qwen2_5_vl"] = AutoModelForImageTextToText
+AUTOMODEL_FACTORY["qwen2_vl"] = AutoModelForImageTextToText
+AUTOMODEL_FACTORY["qwen2_5_omni"] = AutoModelForTextToWaveform
+AUTOMODEL_FACTORY["llava"] = AutoModelForImageTextToText
+AUTOMODEL_FACTORY["internvl"] = AutoModelForImageTextToText
+AUTOMODEL_FACTORY["gemma3"] = AutoModelForImageTextToText
+AUTOMODEL_FACTORY["smolvlm"] = AutoModelForImageTextToText
+AUTOMODEL_FACTORY["mistral3"] = AutoModelForImageTextToText
+AUTOMODEL_FACTORY["llama4"] = AutoModelForImageTextToText
+
+
+def resolve_model_class(model_name: str) -> nn.Module:
+    if model_name.lower() in AUTOMODEL_FACTORY.keys():
+        return AUTOMODEL_FACTORY[model_name.lower()]
+    return AutoModelForCausalLM
 
 
 def sliding_window_overwrite(model_name: str) -> dict[str, Any]:
